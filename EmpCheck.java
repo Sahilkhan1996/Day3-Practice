0a72d6069d30a67cace51177c0cc60c8043e4abb
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-07-02 21:07:10 +0530

    Final Employee Wage

--- EmpCheck.java
+++ EmpCheck.java
@@ -1,38 +1,36 @@
 import java.util.Random;
 
 public class EmpCheck {
-	static int fullday = 8;
-	static int wageperhour = 20;
-	static int parttimehour = 4;
-	static int NumberOfWorkingDays=20;
-    static int totalempwage;
+	static int fullday=8;
+    static int wageperhour = 20;
+    static int parttimehour = 4;
+    static int NumberOfWorkingDays=20;
+    static int totalemppwage;
+    static int maxhours = 100;
     
-	public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        int totalHrs = 0;
+        for (int i = 0; totalHrs<maxhours &&  i < NumberOfWorkingDays; i++)
+        {
+            Random check1 = new Random();
+            int presentorabsent1 = check1.nextInt(0, 3);
+           
 
-		Random check1 = new Random();
-		int presentorabsent1 = check1.nextInt(0, 3);
-		System.out.println(presentorabsent1);
-		switch (presentorabsent1) {
-		case 0:
+            switch (presentorabsent1)
+            {
+                case 1:
+                    totalemppwage = totalemppwage + parttimehour * wageperhour;
+                    totalHrs = totalHrs+parttimehour;
+                    break;
+                case 2:
+                    totalemppwage = totalemppwage + fullday * wageperhour;
+                    totalHrs =totalHrs+ parttimehour;
+                    break;
+                case 3: System.out.println("Employee is absent"); break;
 
-			System.out.println("Employee is present");
-			int DailyEmpWage = fullday * wageperhour;
-			totalempwage=NumberOfWorkingDays*DailyEmpWage;
-			System.out.println("Total wage for Full day working of Daily Wage of the Employee is: " +totalempwage);
-			break;
-		case 1:
-			System.out.println("Employee is present");
-			int PartTimeDailyEmpWage = parttimehour * wageperhour;
-			totalempwage=NumberOfWorkingDays*PartTimeDailyEmpWage;
-			System.out.println("Total wage for Part time working Daily Wage of the Employee is: " +totalempwage);
-
-			break;
-		case 2:
-			System.out.println("Employee is absent");
-			System.out.println("Total wage is zero because Emp is absent");
-
-			break;
-
-		}
-	}
+            }
+        }
+        System.out.println("total Emp wage is: " +totalemppwage+" for total hours:"+totalHrs );
+    }
 }
