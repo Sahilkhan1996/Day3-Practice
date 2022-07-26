ad02acb4b634b61a5c70dd537149b057f02c71c5
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-07-26 21:19:56 +0530

    Using class level var and class level method

--- EmpCheck.java
+++ EmpCheck.java
@@ -1,36 +1,45 @@
 import java.util.Random;
 
 public class EmpCheck {
-	static int fullday=8;
-    static int wageperhour = 20;
-    static int parttimehour = 4;
-    static int NumberOfWorkingDays=20;
-    static int totalemppwage;
-    static int maxhours = 100;
-    
-    public static void main(String[] args)
-    {
-        int totalHrs = 0;
-        for (int i = 0; totalHrs<maxhours &&  i < NumberOfWorkingDays; i++)
-        {
-            Random check1 = new Random();
-            int presentorabsent1 = check1.nextInt(0, 3);
-           
+	static int fullday = 8;
+	static int wageperhour = 20;
+	static int parttimehour = 4;
+	static int NumberOfWorkingDays = 20;
+	static int totalemppwage;
+	static int maxhours = 100;
+	static int presentorabsent1, totalHrs = 0;
 
-            switch (presentorabsent1)
-            {
-                case 1:
-                    totalemppwage = totalemppwage + parttimehour * wageperhour;
-                    totalHrs = totalHrs+parttimehour;
-                    break;
-                case 2:
-                    totalemppwage = totalemppwage + fullday * wageperhour;
-                    totalHrs =totalHrs+ parttimehour;
-                    break;
-                case 3: System.out.println("Employee is absent"); break;
+	public static int checkEmp() {
+		Random check1 = new Random();
+		presentorabsent1 = check1.nextInt(0, 3);
+		return presentorabsent1;
+	}
 
-            }
-        }
-        System.out.println("total Emp wage is: " +totalemppwage+" for total hours:"+totalHrs );
-    }
+	public static void checkingifpresentorabsent() {
+		switch (presentorabsent1) {
+		case 1:
+			totalemppwage = totalemppwage + parttimehour * wageperhour;
+			totalHrs = totalHrs + parttimehour;
+			break;
+		case 2:
+			totalemppwage = totalemppwage + fullday * wageperhour;
+			totalHrs = totalHrs + parttimehour;
+			break;
+		case 3:
+			System.out.println("Employee is absent");
+			break;
+
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		for (int i = 0; totalHrs < maxhours && i < NumberOfWorkingDays; i++) {
+			checkEmp();
+			checkingifpresentorabsent();
+
+		}
+		System.out.println("total Emp wage is: " + totalemppwage + " for total hours:" + totalHrs);
+	}
 }
