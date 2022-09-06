bae5fa0241093cecd4c5c2ec8b4cfe7a610d6562
sahilkhan1996 <sahilkhan199655@gmail.com>
2022-09-06 18:03:46 +0530

    Employee Wage with the interface

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,62 +1,65 @@
 
-public class EmpWageBuilder {
+public class EmpWageBuilder implements WageCalculate{
 
-//    CompanyEmpWage[] arr = new CompanyEmpWage[arrayLength];
+    @Override
+    public void totalWage(CompanyEmpWage[] arr){
 
-	public void totalWage(CompanyEmpWage[] arr) {
+        int dayCount = WageCalculate.dayCount;
+        int hourCount = WageCalculate.hourCount;
+        int totalWage = WageCalculate.totalWage;
+        int dayPresent = WageCalculate.dayPresent;
 
-		int dayCount = 1;
-		int hourCount = 0;
-		int totalWage = 0;
-		int dayPresent = 0;
+        for(int i = 0 ; i<arr.length;i++) {
+            while (dayCount <= arr[i].totalWorkDay && hourCount <= arr[i].totalWorkHour) {
 
-		for (int i = 0; i < arr.length; i++) {
-			while (dayCount <= arr[i].totalWorkDay && hourCount <= arr[i].totalWorkHour) {
+                int dailyWage = 0;
+                int attendance = (int) (Math.random() * 10) % 3;
 
-				int dailyWage = 0;
-				int attendance = (int) (Math.random() * 10) % 3;
+                switch (attendance) {
+                    case 1:
 
-				switch (attendance) {
-				case 1:
-
-					if (hourCount + arr[i].fullDayHour <= arr[i].totalWorkHour) {
+                        if(hourCount + arr[i].fullDayHour <= arr[i].totalWorkHour){
 
 //                            System.out.println("Employee is present Full Time");
-						hourCount = hourCount + arr[i].fullDayHour;
-						dailyWage = arr[i].wagePerHour * arr[i].fullDayHour;
-						dayPresent++;
+                            hourCount = hourCount + arr[i].fullDayHour;
+                            dailyWage = arr[i].wagePerHour * arr[i].fullDayHour;
+                            dayPresent++;
 
-					}
-					break;
-				case 2:
+                        }
+                        break;
+                    case 2:
 
-					if (hourCount + arr[i].partDayHour <= arr[i].totalWorkHour) {
+                        if(hourCount + arr[i].partDayHour <= arr[i].totalWorkHour){
 
 //                            System.out.println("Employee is present Part Time");
-						hourCount = hourCount + arr[i].partDayHour;
-						dailyWage = arr[i].wagePerHour * arr[i].partDayHour;
-						dayPresent++;
+                            hourCount = hourCount + arr[i].partDayHour;
+                            dailyWage = arr[i].wagePerHour * arr[i].partDayHour;
+                            dayPresent++;
 
-					}
-					break;
-				default:
+                        }
+                        break;
+                    default:
 //                        System.out.println("Employee is absent");
-				}
+                }
 
 //                System.out.println("Daily Wage on Day :" + dayCount + " is :" + dailyWage);
-				totalWage = totalWage + dailyWage;
-				dayCount++;
-			}
+                totalWage = totalWage + dailyWage;
+                dayCount++;
+            }
 
-			System.out.println("For company " + arr[i].companyName + ". \n" + "In one month of " + (dayCount - 1)
-					+ " working days, \n" + "employees present for " + dayPresent + " days and \n" + "worked for "
-					+ hourCount + " hours. \n" + "earn wages: " + totalWage + "\n");
 
-			arr[i].setTotalWage(totalWage);
-			dayCount = 1;
-			dayPresent = 0;
-			hourCount = 0;
-		}
+            System.out.println("For company " + arr[i].companyName + ". \n" +
+                    "In one month of " + (dayCount - 1) + " working days, \n"
+                    + "employees present for " + dayPresent + " days and \n"
+                    + "worked for " + hourCount + " hours. \n" +
+                    "earn wages: " + totalWage +"\n");
 
-	}
+            arr[i].setTotalWage(totalWage);
+            dayCount = WageCalculate.dayCount;
+            hourCount = WageCalculate.hourCount;
+            dayPresent = WageCalculate.dayPresent;
+
+        }
+
+    }
 }
